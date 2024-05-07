067690cc1c205373b1212ead3895f91d27fa45a7
lucasinho11 <lucas.lubasinski11@gmail.com>
2024-05-07 14:37:26 +0200

    Added users page :new:

---
+++ pages/users/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <pre>{{users}}</pre>
+    </div>
+</template>
+
+<script setup lang="ts">
+const store = useUsersStore();
+const {users} = storeToRefs(store)
+store.getUsers();
+</script>
+
+<style scoped>
+
+</style>
